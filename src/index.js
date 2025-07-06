348c49d5f6d46e5541585f3db3b9a34ebc2fcc8b
Lars <lars.dehairs@gmail.com>
2025-07-06 14:07:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./styles.css";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/serviceWorker.js").catch(console.error);
+  });
+}
