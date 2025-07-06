8722989845e068986da0b5934ce305ee2cecc833
Lars <lars.dehairs@gmail.com>
2025-07-06 14:13:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/serviceWorker.js").catch(console.error);
